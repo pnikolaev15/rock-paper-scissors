75f30aea43731813e6bf5486048ded18819a6a7d
Pavel Nikolaev <pnikolaev15@yahoo.com>
2022-08-25 13:17:06 +0300

    add querySelector and listeners for elements

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const playerScoreIcon = document.querySelector('#playerScore');
+const computerScoreIcon = document.querySelector('#computerScore');
+
 const getComputerChoice = () => {
   const choices = ['rock', 'paper', 'scissors'];
   const randomNum = Math.floor(Math.random() * 3);
@@ -26,27 +32,52 @@ const game = () => {
   let playerScore = 0;
   let computerScore = 0;
 
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt('Rock, paper, or scissors?');
+  const isGameEnd = () => playerScore === 5 || computerScore === 5;
+
+  rockBtn.addEventListener('click', () => onWeaponBtnClick('rock'));
+  paperBtn.addEventListener('click', () => onWeaponBtnClick('paper'));
+  scissorsBtn.addEventListener('click', () => onWeaponBtnClick('scissors'));
+  
+  const onWeaponBtnClick = (playerSelection) => {
+    if (isGameEnd()) {
+      playerScore = 0;
+      computerScore = 0;
+      playerScoreIcon.innerHTML = playerScore;
+      computerScoreIcon.innerHTML = computerScore;
+    }
+
     const computerSelection = getComputerChoice();
     const res = playRound(playerSelection, computerSelection);
-
     if (res === 1) {
-      playerScore += 1;
+      playerScore++;
+      playerScoreIcon.innerHTML = playerScore;
     } else if (res === -1) {
-      computerScore += 1;
-    } else {
-      continue;
+      computerScore++;
+      computerScoreIcon.innerHTML = computerScore;
     }
-  }
+  };
+
+  // for (let i = 0; i < 5; i++) {
+  //   const playerSelection = prompt('Rock, paper, or scissors?');
+  //   const computerSelection = getComputerChoice();
+  //   const res = playRound(playerSelection, computerSelection);
+
+  //   if (res === 1) {
+  //     playerScore += 1;
+  //   } else if (res === -1) {
+  //     computerScore += 1;
+  //   } else {
+  //     continue;
+  //   }
+  // }
   
-  if (playerScore > computerScore) {
-    console.log(`You won! You: ${playerScore}, PC: ${computerScore}`);
-  } else if (computerScore > playerScore) {
-    console.log(`You lose! You: ${playerScore}, PC: ${computerScore}`);
-  } else {
-    console.log(`Tie game! You: ${playerScore}, PC: ${computerScore}`);
-  }
+  // if (playerScore > computerScore) {
+  //   console.log(`You won! You: ${playerScore}, PC: ${computerScore}`);
+  // } else if (computerScore > playerScore) {
+  //   console.log(`You lose! You: ${playerScore}, PC: ${computerScore}`);
+  // } else {
+  //   console.log(`Tie game! You: ${playerScore}, PC: ${computerScore}`);
+  // }
 };
 
 game();
